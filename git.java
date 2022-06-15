ab6b38b9dbb0b8e62ae7783ee96b21b34b91259d
mohita <sachdevamohita5@gmail.com>
2022-06-15 18:57:50 +0530

    [mohita] month and date

---
+++ git.java
@@ -0,0 +1,12 @@
+puclic class ABC {
+public static void main (STring [])
+int num =123,reversed=0;
+while (num!= 0){int digit =num%10;
+reversed = reversed *10
+num/=10;
+}
+System.out.println ("reversed")
+}
+}
+
+
---
+++ random.sh
@@ -0,0 +1,6 @@
+
+#! /bin/bash
+        dice1=$((RANDOM%6))
+        dice2=$((RANDOM%5))
+        sum=$((dice1+dice2))
+        echo=$sum

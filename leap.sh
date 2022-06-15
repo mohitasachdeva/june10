38e476b053f14f052a24eaf88f4b638b7cd067ac
mohita <sachdevamohita5@gmail.com>
2022-06-15 20:01:02 +0530

    [mohita] program of leap year

---
+++ leap.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+read -p  "Enter year:- " year
+
+if (( (year % 400 == 0 ) )) 
+then
+    echo $year "is a Leap Year" ;
+elif (( ( year % 100 == 0 ) ))
+then 
+    echo $year "is not a Leap Year" ;
+elif (( ( year % 4 == 0  ) ))
+then
+    echo $year "is a Leap Year" ;
+else 
+    echo $year "is not a Leap Year" ;
+fi 
+
+
+

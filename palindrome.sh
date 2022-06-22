832a6504b1a7026f271f6a3a201d33e4777ce332
mohita <sachdevamohita5@gmail.com>
2022-06-22 20:09:23 +0530

    [mohita] palindrome number

---
+++ palindrome.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+read -p "enter a number" num
+pal() 
+{
+# Storing the remainder
+s=0
+   
+# Store number in reverse 
+# order
+rev=""   
+# Store original number 
+# in another variable
+temp=$num
+   
+while [ $num -gt 0 ]
+do
+    # Get Remainder
+    s=$(( $num % 10 ))  
+      
+    # Get next digit
+    num=$(( $num / 10 )) 
+      
+    # Store previous number and
+    # current digit in reverse 
+    rev=$( echo ${rev}${s} ) 
+done
+if [ $temp -eq $rev ];
+then
+    echo "Number is palindrome"
+else
+    echo "Number is NOT palindrome"
+fi
+}
+pal

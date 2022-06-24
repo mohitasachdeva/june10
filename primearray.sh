bc56ef3b8d5e23a9b4f4e0eea2e569944be24c12
mohita <sachdevamohita5@gmail.com>
2022-06-24 13:08:56 +0530

    [mohita] store prime factors in array

---
+++ primearray.sh
@@ -0,0 +1,13 @@
+#!/bin/bash  -x
+read -p "enter a number" num
+declare -A  prime
+for (( i=2; i<=$num; i++ ));
+do
+    while [ $((num%$i)) == 0 ];
+do
+        echo $i
+        num=$((num/$i))
+prime[$num]=$i
+    done
+done
+
